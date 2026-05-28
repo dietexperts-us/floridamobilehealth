2d1fd377f5994c35a9af360a6ff530e5845d20d7
Florida Mobile Health <info@floridamobilehealth.com>
2026-05-28 16:26:28 +0000

    Fix server.js to explicitly serve sitemap.xml and robots.txt with correct MIME types

--- server.js
+++ server.js
@@ -12,8 +12,27 @@ app.use(express.static(path.join(__dirname), {
     if (filePath.endsWith('.js')) {
       res.setHeader('Content-Type', 'application/javascript');
     }
+    if (filePath.endsWith('.xml')) {
+      res.setHeader('Content-Type', 'application/xml');
+      res.setHeader('X-Robots-Tag', 'noindex');
+    }
+    if (filePath.endsWith('.txt')) {
+      res.setHeader('Content-Type', 'text/plain');
+    }
   }
 }));
+
+// Explicit sitemap route
+app.get('/sitemap.xml', (req, res) => {
+  res.setHeader('Content-Type', 'application/xml');
+  res.sendFile(path.join(__dirname, 'sitemap.xml'));
+});
+
+// Explicit robots.txt route
+app.get('/robots.txt', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.sendFile(path.join(__dirname, 'robots.txt'));
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
